c3218af13ab96f601817299a81bea8262efec336
hangxigood <hangxigood@gmail.com>
2020-05-16 00:21:10 +0800

    使用 Docker 进行项目部署

--- blog/settings.py
+++ blog/settings.py
@@ -19,7 +19,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ''
+#SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
@@ -91,8 +91,8 @@ DATABASES = {
         'NAME': 'blog',
         'HOST': 'blog_database',
         'PORT': 3306,
-        'USER': '',
-        'PASSWORD': '',
+        'USER': 'root',
+        'PASSWORD': 'sheep',
     }
 }
 
